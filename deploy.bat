b3b180515b411cd19b99b4111d5ab78237890a95
MAHEKKEVAT <mahekforever2003@gmail.com>
2026-04-18 17:59:46 +0530

    test

--- deploy.bat
+++ deploy.bat
@@ -1,17 +1,6 @@
 @echo off
+chcp 65001 >nul
 
-
-echo ================================
-echo Pushing to GitHub...
-echo ================================
-
-git add .
-git commit -m "Auto deploy update"
-git push origin main
-
-echo ================================
-echo DONE 🚀 Your site is updating...
-echo ================================
 echo ================================
 echo Building Flutter Web...
 echo ================================
@@ -23,5 +12,17 @@ echo Copying files to docs...
 echo ================================
 
 xcopy build\web* docs\ /E /H /Y
+
+echo ================================
+echo Pushing to GitHub...
+echo ================================
+
+git add .
+git commit -m "Update site"
+git push origin main
+
+echo ================================
+echo DONE 🚀
+echo ================================
 
 pause
